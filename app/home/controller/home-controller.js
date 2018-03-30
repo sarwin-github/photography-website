5fff90e8d17d7d8c1646b9e7b69b3666530a7e76
Sherwin Dapito <dapito.sherwin@yahoo.com>
2018-03-30 11:34:54 +0800

    Added contact form and modified headers and fonts

--- app/home/controller/home-controller.js
+++ app/home/controller/home-controller.js
@@ -1,8 +1,54 @@
 const async = require("async");
+const nodemailer = require('nodemailer');
+const csrf       = require('csurf');
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This will make a get request and render the index page for logging in a professional or client
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 module.exports.getHome = (req, res) => {
-	res.render('home/home.ejs', { success: true });
+	res.render('home/home.ejs', { 
+		success: true, 
+		error: req.flash('error'),
+		message: req.flash('message'),
+		csrfToken: req.csrfToken()
+	});
+};
+
+module.exports.sendContactForm = (req, res) => {
+	async.parallel({
+		// get the list of centers
+	    getContactForm: (callback) => {
+	    	let	mailOptions, smtpTransport;
+
+	    	//EMail SMTP Configuration
+	    	smtpTransport = nodemailer.createTransport({
+	    		host: "smtp.gmail.com", //Host name
+	    		secureConnection: true, //use Secure Shell
+	    		port: 465, //port for secure SMTP
+	    		auth: {
+	    			user: process.env.gmail_email,
+	    			pass: process.env.gmail_password
+	    		}
+	    	});
+
+	    	mailOptions = {
+	    		to: process.env.gmail_email,
+	    		from: req.body.name + ' <' + req.body.email + '>' ,
+	    		subject: req.body.subject, 
+	    		text: req.body.message + '\n \nFrom: ' + req.body.name  + ' \nEmail: ' + req.body.email
+	    	}
+
+	    	smtpTransport.sendMail(mailOptions, err =>{
+    			return callback(err);
+    		});
+	    }
+	}, (err, data) => {
+	    if(err){
+	    	req.flash('error', err);
+	        return res.redirect('/');
+	    } 
+
+	    req.flash('message', 'Email has been successfully sent.');
+	    res.redirect('/');
+	});	
 };
--- app/home/route/home-route.js
+++ app/home/route/home-route.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const router  = express();
+const express   = require('express');
+const csrf      = require('csurf');
+const router    = express();
 
 const homeController = require('../controller/home-controller');
 
+const csrfProtection = csrf();
+router.use(csrfProtection);
+
 /* Get The home page with list of fitness option */
 router.route('/').get(homeController.getHome);
+router.route('/').post(homeController.sendContactForm);
 
 module.exports = router;
